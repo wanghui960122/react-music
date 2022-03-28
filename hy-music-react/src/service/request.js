b349a93cd82a80443a685c53f2754a57dbd23573
pdf8b4 <1205058003@qq.com>
2022-03-28 13:38:46 +0800

    update

--- hy-music-react/src/service/request.js
+++ hy-music-react/src/service/request.js
@@ -2,35 +2,39 @@ import axios from "axios";
 
 export const httpRequest = axios.create({
   timeout: 30000,
-  baseURL: 'http://123.207.32.32:9001'
-})
-
-httpRequest.interceptors.request.use(config => {
-  // 1.发送网络请求时, 在界面的中间位置显示Loading的组件
-
-  // 2.某一些请求要求用户必须携带token, 如果没有携带, 那么直接跳转到登录页面
-
-  // 3.params/data序列化的操作
-
-  return config;
-}, err => {
-
+  baseURL: "/backend",
 });
 
-httpRequest.interceptors.response.use(res => {
-  return res.data;
-}, err => {
-  if (err && err.response) {
-    switch (err.response.status) {
-      case 400:
-        console.log("请求错误");
-        break;
-      case 401:
-        console.log("未授权访问");
-        break;
-      default:
-        console.log("其他错误信息");
+httpRequest.interceptors.request.use(
+  (config) => {
+    // 1.发送网络请求时, 在界面的中间位置显示Loading的组件
+
+    // 2.某一些请求要求用户必须携带token, 如果没有携带, 那么直接跳转到登录页面
+
+    // 3.params/data序列化的操作
+
+    return config;
+  },
+  (err) => {}
+);
+
+httpRequest.interceptors.response.use(
+  (res) => {
+    return res.data;
+  },
+  (err) => {
+    if (err && err.response) {
+      switch (err.response.status) {
+        case 400:
+          console.log("请求错误");
+          break;
+        case 401:
+          console.log("未授权访问");
+          break;
+        default:
+          console.log("其他错误信息");
+      }
     }
+    return err;
   }
-  return err;
-});
+);
